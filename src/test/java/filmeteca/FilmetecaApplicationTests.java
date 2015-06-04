52890cfb011c648caf23bc277e36c57b60dd30e2
unknown <guilherme.ferro@SEINFO-143578.quinino.fiocruz.br>
2015-06-03 22:02:24 -0300

    Inicio do commit junto do ignore

---
+++ src/test/java/filmeteca/FilmetecaApplicationTests.java
@@ -0,0 +1,18 @@
+package filmeteca;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = FilmetecaApplication.class)
+@WebAppConfiguration
+public class FilmetecaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
